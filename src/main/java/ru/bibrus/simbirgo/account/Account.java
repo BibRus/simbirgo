0c366844175308dac85c9471588d3764c047e84b
Bibaev Ruslan <bibaevsurlan@gmail.com>
2023-10-21 03:03:48 +0400

    feat: update payment implementation

--- src/main/java/ru/bibrus/simbirgo/account/Account.java
+++ src/main/java/ru/bibrus/simbirgo/account/Account.java
@@ -1,5 +1,33 @@
 package ru.bibrus.simbirgo.account;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Getter
+    @Setter
+    private boolean isAdmin;
+
+    private double balance;
+
+    public final void topUpBalance(double amount) {
+        this.balance += amount;
+    }
 
 }
--- src/main/java/ru/bibrus/simbirgo/payment/PaymentController.java
+++ src/main/java/ru/bibrus/simbirgo/payment/PaymentController.java
@@ -1,5 +1,34 @@
 package ru.bibrus.simbirgo.payment;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.bibrus.simbirgo.account.Account;
+import ru.bibrus.simbirgo.account.AccountRepository;
+
+@RestController
 public class PaymentController {
+
+    private static final long MONETARY_UNITS = 250_000;
+
+    private final AccountRepository accountRepository;
+
+    public PaymentController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Transactional
+    @PostMapping("/api/Payment/Hesoyam/{accountId}")
+    public ResponseEntity<Account> topUpAccountBalance(@PathVariable long accountId) {
+        Account account = accountRepository.getReferenceById(accountId);
+        if (account.isAdmin()) {
+            account.topUpBalance(MONETARY_UNITS);
+            accountRepository.save(account);
+        }
+        return ResponseEntity.ok(account);
+    }
 
 }
