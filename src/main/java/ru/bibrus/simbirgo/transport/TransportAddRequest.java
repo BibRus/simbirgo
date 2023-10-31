70adf01f6599eb77855786e06cac30123ce168e6
Bibaev Ruslan <bibaevsurlan@gmail.com>
2023-10-31 20:33:37 +0400

    feat: update transport domain

--- src/main/java/ru/bibrus/simbirgo/transport/TransportAddRequest.java
+++ src/main/java/ru/bibrus/simbirgo/transport/TransportAddRequest.java
@@ -1,12 +1,5 @@
 package ru.bibrus.simbirgo.transport;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,19 +7,12 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "transports")
-public class Transport {
-
-    @Id
-    @GeneratedValue
-    private long id;
+@NoArgsConstructor
+public class TransportAddRequest {
 
     private boolean canBeRented;
 
-    @Enumerated(EnumType.STRING)
     private TransportType transportType;
 
     private String model;
--- src/main/java/ru/bibrus/simbirgo/transport/TransportController.java
+++ src/main/java/ru/bibrus/simbirgo/transport/TransportController.java
@@ -1,5 +1,39 @@
 package ru.bibrus.simbirgo.transport;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/Transport")
 public class TransportController {
+
+    @PostMapping
+    public String addTransport(@RequestBody TransportAddRequest request) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public String updateTransport(@PathVariable long id) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public String getInformationById(@PathVariable long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTransportById(@PathVariable long id) {
+        return null;
+    }
 
 }
